ed43d0f143c2d23648b4befa9c617382f9d60219
Aditya Neve <aditya.neve@gmail.com>
2021-10-20 12:14:53 +0530

    debounce added

--- Import-Export/food-app-1/components/navbar.js
+++ Import-Export/food-app-1/components/navbar.js
@@ -3,7 +3,7 @@ function navbar() {
         `
         <h3><a href="./index.html">HOME</a></h3>
         <div id="options">
-            <input id="search" type="text" oninput="debounce(main, 1000)" placeholder="Search a receipe">
+            <input id="search" type="text" oninput="" placeholder="Search a receipe">
             <button id="receipeOfTheDay" type="button"><a href="./receipeOfTheDay.html">Get Receipe of the Day</a></button>
             <button id="latestReceipe" type="button"><a href="./latestReceipe.html">Latest Receipe</a></button>
         </div>
